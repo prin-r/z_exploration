b58556f3b8b0aa538c2874541d528b065b09dc8e
prin-r <q@bandprotocol.com>
2022-11-11 20:31:23 +0700

    -

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # z_exploration
+
+
+discover 1
+```shell
+if n is odd number then 
+n - 1 is even then
+sum (k = 1 to n - 1) 1/(1+e^(2*k*i*pi/5))
+is equal to (n-1)/2
+```
--- main.py
+++ main.py
@@ -11,6 +11,22 @@ def cal_directly(n,s):
 
 
 def create_table(n, m):
-  print(1, sorted([i%m for i in range(1, n+1)]))
+  # print(1, sorted([i%m for i in range(1, n+1)]))
+  tmp = [[i % m for i in range(1, n + 1)]]
   for i in range(2,m):
-    print(i, sorted([(j*i)%m for j in range(1, n+1)]))
+    # print(i, sorted([(j*i)%m for j in range(1, n+1)]))
+    tmp.append([(j * i) % m for j in range(1, n + 1)])
+
+  if m % 2 == 0:
+    for t in tmp:
+      if m >> 1 not in t:
+        print(t)
+  else:
+    for t in tmp:
+      print(t)
+
+  print("-"*n*3)
+
+
+create_table(19, 15)
+print(cal_directly(19, 15))
